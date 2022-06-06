ae83b36fe3568eeac195f540da12a2b27881201a
Oneul <toda1213@naver.com>
2022-06-07 00:30:19 +0900

    chapter2-2
    순수함수, 재귀함수, lazy

--- src/chapter2/funprog.kt
+++ src/chapter2/funprog.kt
@@ -44,6 +44,87 @@ class PrintMachine<T>: Machine<T> {
     }
 }
 
+// 재귀함수
+fun factorial(n: Long): Long {
+    var result = 1L
+    for (i in 1..n) {
+        result *= i
+    }
+
+    return result
+}
+
+fun functionalFactorial(n: Long): Long {
+    fun go(n: Long, acc: Long): Long {
+        return if (n <= 0) {
+            acc
+        } else {
+            go(n - 1, n * acc)
+        }
+    }
+    return go(n, 1)
+}
+
+fun tailrecFactorial(n: Long): Long {
+    tailrec fun go(n: Long, acc: Long): Long {
+        return if (n <= 0) {
+            acc
+        } else {
+            go(n - 1, n * acc)
+        }
+    }
+    return go(n, 1)
+}
+
+fun executionTime(body: () -> Unit): Long {
+    val startTime = System.nanoTime()
+    body()
+    val endTime = System.nanoTime()
+    return endTime - startTime
+}
+
+fun fib(n: Long): Long {
+    return when(n) {
+        0L -> 0
+        1L -> 1
+        else -> {
+            var a = 0L
+            var b = 1L
+            var c = 0L
+            for (i in 2..n) {
+                c = a + b
+                a = b
+                b = c
+            }
+            c
+        }
+    }
+}
+
+fun functionalFib(n: Long): Long {
+    fun go(n: Long, prev: Long, cur: Long): Long {
+        return if (n == 0L) {
+            prev
+        } else {
+            go(n - 1, cur, prev + cur)
+        }
+    }
+
+    return go(n, 0, 1)
+}
+
+fun tailrecFib(n: Long): Long {
+    tailrec fun go(n: Long, prev: Long, cur: Long): Long {
+        return if (n == 0L) {
+            prev
+        } else {
+            go(n - 1, cur, prev + cur)
+        }
+    }
+
+    return go(n, 0, 1)
+}
+
 fun main(args: Array<String>) {
     println(capitalize("hello world!"))
     println(transform("kotlin", capitalize))
@@ -71,4 +152,23 @@ fun main(args: Array<String>) {
     useMachine(5) { i ->
         println(i)
     }
+
+    println("factorial : " + executionTime { factorial(20) })
+    println("functionalFactorial : " + executionTime { functionalFactorial(20) })
+    println("tailrecFactorial : " + executionTime { tailrecFactorial(20) })
+
+    println("fib : " + executionTime { fib(93) })
+    println("functionalFib : " + executionTime { functionalFib(93) })
+    println("tailrecFib : " + executionTime { tailrecFib(93) })
+
+    // lazy
+    val i by lazy {
+        println("느긋한 계산법")
+        1
+    }
+
+    println("i 사용 전")
+    println(i)
+
+    val size = listOf({2+1}, {3*2}, {1/0}, {5-4}).size
 }
