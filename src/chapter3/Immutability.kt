acb41e06212433528a23bff2ca304c108c5620b8
Oneul <toda1213@naver.com>
2022-06-11 01:23:22 +0900

    Chapter 3-2
    불변성의 종류, 불변 컬렉션

--- src/chapter3/Immutability.kt
+++ src/chapter3/Immutability.kt
@@ -12,6 +12,14 @@ object MutableVal {
 //    }
 }
 
+// 참조 불변
+class MutableObj {
+    var value =""
+    override fun toString(): String {
+        return "MutableObj(value='$value')"
+    }
+}
+
 fun main(args: Array<String>) {
 //    var x: String = "abc"
 //    var y = x.capitalize()
@@ -23,4 +31,24 @@ fun main(args: Array<String>) {
     println("1번째 호출 ${MutableVal.myString}")
     println("2번째 호출 ${MutableVal.myString}")
     println("3번재 호출 ${MutableVal.myString}")
+
+    // 참조 불변
+    val mutableObj: MutableObj = MutableObj()
+    println("MutableObj $mutableObj")
+    mutableObj.value = "changed"
+    println("MutableObj $mutableObj")
+
+    val list = mutableListOf("a", "b", "c", "d", "e")
+    println(list)
+    list.add("f")
+    println(list)
+
+    // 불변 컬렉션
+    val immutableList = listOf(1, 2, 3, 4, 5, 6, 7)
+    println("불변 리스트 $immutableList")
+    val mutableList: MutableList<Int> = immutableList.toMutableList()
+    println("가변 리스트 $mutableList")
+    mutableList.add(8)
+    println("추가 후의 가변 리스트 $mutableList")
+    println("추가 후의 불변 리스트 $immutableList")
 }
