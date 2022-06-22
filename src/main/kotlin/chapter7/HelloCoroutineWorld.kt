42651c11d0caac4ae2ff373a545e769453c596b3
Oneul <toda1213@naver.com>
2022-06-23 02:49:27 +0900

    chapter 7-3
    Hello, Coroutine World! - 기본 예제

---
+++ src/main/kotlin/chapter7/HelloCoroutineWorld.kt
@@ -0,0 +1,14 @@
+package chapter7
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = launch {
+        delay(1000)
+        println("World")
+    }
+    print("Hello ")
+    job.join()
+}
