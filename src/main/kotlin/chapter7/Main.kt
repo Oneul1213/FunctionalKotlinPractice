f29689af76a9a8a07ae89b13ae528be7e4903bc7
Oneul <toda1213@naver.com>
2022-06-26 01:59:09 +0900

    chapter 7-6
    동기 구현 - Main.kt 추가

---
+++ src/main/kotlin/chapter7/Main.kt
@@ -0,0 +1,32 @@
+package chapter7
+
+import chapter5.inTime
+
+fun main() {
+    fun execute(userService: UserService, id: Int) {
+        val (fact, time) = inTime {
+            userService.getFact(id)
+        }
+        println("진실 = $fact")
+        println("시간 = $time ms")
+    }
+
+    val userClient = MockUserClient()
+    val factClient = MockFactClient()
+    val userRepository = MockUserRepository()
+    val factRepository = MockFactRepository()
+    val userService = SynchronousUserService(userClient,
+        factClient,
+        userRepository,
+        factRepository)
+
+    execute(userService, 1)
+    execute(userService, 2)
+    execute(userService, 1)
+    execute(userService, 2)
+    execute(userService, 3)
+    execute(userService, 4)
+    execute(userService, 5)
+    execute(userService, 10)
+    execute(userService, 100)
+}
