1dfad4e5a0a30e9d48bb6002619ad2188a3018a7
Oneul <toda1213@naver.com>
2022-07-08 00:59:32 +0900

    chapter 9-6-2
    구독 및 폐기 - DisposableExample.kt 추가

---
+++ src/main/kotlin/chapter9/DisposableExample.kt
@@ -0,0 +1,41 @@
+package chapter9
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
+
+fun main() {
+    val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
+    val observer = object: Observer<Long> {
+
+        lateinit var disposable: Disposable
+
+        override fun onSubscribe(d: Disposable) {
+            disposable = d
+        }
+
+        override fun onNext(item: Long) {
+            println("받음 $item")
+            if (item >= 10 && !disposable.isDisposed) {
+                disposable.dispose()
+                println("정리됨")
+            }
+        }
+
+        override fun onError(e: Throwable) {
+            println("에러 ${e.message}")
+        }
+
+        override fun onComplete() {
+            println("완료")
+        }
+    }
+
+    runBlocking {
+        observable.subscribe(observer)
+        delay(1500)
+    }
+}
