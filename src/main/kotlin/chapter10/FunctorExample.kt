f41aa08311d88991e5b087f01daa54a74d48043c
Oneul <toda1213@naver.com>
2022-07-08 15:50:16 +0900

    chapter 10-1-1
    펑터 - FunctorExample.kt 추가

---
+++ src/main/kotlin/chapter10/FunctorExample.kt
@@ -0,0 +1,38 @@
+package chapter10
+
+import chapter4.add
+import java.util.*
+
+sealed class Option<out T> {
+    object None: Option<Nothing>() {
+        override fun toString() = "None"
+    }
+
+    data class Some<out T>(val value: T): Option<T>()
+
+    companion object
+}
+
+fun <T, R> Option<T>.map(transform: (T) -> R): Option<R> = when (this) {
+    Option.None -> Option.None
+    is Option.Some -> Option.Some(transform(value))
+}
+
+fun <A, B, C> ((A) -> B).map(transform: (B) -> C): (A) -> C = { t ->
+    transform(this(t))
+}
+
+fun main() {
+    listOf(1, 2, 3)
+        .map { i -> i * 2 }
+        .map(Int::toString)
+        .forEach(::println)
+
+    println(Option.Some("Kotlin").map(String::toUpperCase))
+    println(Option.None.map(String::toUpperCase))
+
+    val add3AndMultiplyBy2: (Int) -> Int = { i: Int -> i + 3 }.map { j -> j * 2 }
+    println(add3AndMultiplyBy2(0))
+    println(add3AndMultiplyBy2(1))
+    println(add3AndMultiplyBy2(2))
+}
