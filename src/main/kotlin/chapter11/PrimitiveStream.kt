c165aced6227e2f8f7a4f38f1e04e034f6613120
Oneul <toda1213@naver.com>
2022-07-09 02:03:00 +0900

    chapter 11-3-2
    프리미티브 스트림 - PrimitiveStream.kt에 doubleStream 추가

--- src/main/kotlin/chapter11/PrimitiveStream.kt
+++ src/main/kotlin/chapter11/PrimitiveStream.kt
@@ -1,9 +1,18 @@
 package chapter11
 
+import java.util.stream.DoubleStream
 import java.util.stream.IntStream
 
 fun main() {
     val intStream = IntStream.range(1, 10)
     val result = intStream.sum()
     println("요소의 합계는 $result 이다")
+
+    val doubleStream = DoubleStream.iterate(1.5) { item -> item*1.3 }
+    val avg = doubleStream
+        .limit(10)
+        .peek {
+            println("아이템 $it")
+        }.average()
+    println("10개 아이템의 평균 $avg")
 }
