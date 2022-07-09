27b5141312d96f9c6753d85b2b751530e84ed4ec
Oneul <toda1213@naver.com>
2022-07-10 02:02:11 +0900

    chapter 11-4-1
    스트림 팩토리 메소드 - 스트림 빌더: StreamFactoryMethod.kt 추가

---
+++ src/main/kotlin/chapter11/StreamFactoryMethod.kt
@@ -0,0 +1,20 @@
+package chapter11
+
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+fun main() {
+    val stream = Stream.builder<String>()
+        .add("아이템 1")
+        .add("아이템 2")
+        .add("아이템 3")
+        .add("아이템 4")
+        .add("아이템 5")
+        .add("아이템 6")
+        .add("아이템 7")
+        .add("아이템 8")
+        .add("아이템 9")
+        .add("아이템 10")
+        .build()
+    println("스트림 ${stream.collect(Collectors.toList())}")
+}
