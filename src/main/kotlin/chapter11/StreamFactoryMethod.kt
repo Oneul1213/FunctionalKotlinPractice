6a1d3b6e6e06591e1f9009bed9952e59b4149aee
Oneul <toda1213@naver.com>
2022-07-10 02:11:09 +0900

    chapter 11-4-2
    스트림 팩토리 메소드 - 빈 스트림 생성: Stream.empty(): Stream.empty() 관련 코드 추가

--- src/main/kotlin/chapter11/StreamFactoryMethod.kt
+++ src/main/kotlin/chapter11/StreamFactoryMethod.kt
@@ -4,6 +4,9 @@ import java.util.stream.Collectors
 import java.util.stream.Stream
 
 fun main() {
+    // 스트림 빌더
+    println("--스트림 빌더--")
+
     val stream = Stream.builder<String>()
         .add("아이템 1")
         .add("아이템 2")
@@ -17,4 +20,11 @@ fun main() {
         .add("아이템 10")
         .build()
     println("스트림 ${stream.collect(Collectors.toList())}")
+
+    // Stream.empty()
+    println("--Stream.empty()--")
+
+    val emptyStream = Stream.empty<String>()
+    val item = emptyStream.findAny()
+    println("아이템 $item")
 }
