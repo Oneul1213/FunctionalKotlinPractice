23c8acb9453e6c187299479d8c58ab59592ec898
Oneul <toda1213@naver.com>
2022-07-12 00:22:32 +0900

    chapter 11-5-2
    컬렉터와 Stream.collect: 스트림 수집 - 맵에 수집: Collectors.toMap() - toMap 부분 추가

--- src/main/kotlin/chapter11/CollectorStreamCollect.kt
+++ src/main/kotlin/chapter11/CollectorStreamCollect.kt
@@ -4,7 +4,19 @@ import java.util.stream.Collectors
 import kotlin.streams.asStream
 
 fun main() {
+    // toCollection
+    println("--toCollection--")
     val resultantSet = (0..10).asSequence().asStream()
         .collect(Collectors.toCollection { LinkedHashSet<Int>() })
     println("resultantSet $resultantSet")
+
+    // toMap
+    println("--toMap--")
+    val resultantMap = (0..10).asSequence().asStream()
+        .collect(Collectors.toMap<Int, Int, Int>({
+            it
+        }, {
+            it*it
+        }))
+    println("resultantMap = $resultantMap")
 }
